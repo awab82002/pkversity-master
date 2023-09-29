6ea83e1053530659784e476c5d15aae2b2e82f59
Muhammad-Awab <muhammadawab82002@gmail.com>
2023-09-29 23:43:02 +0500

    add all files

---
+++ User/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Web;
+using User.Authentication;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthenticationCore();
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddSingleton<ClassLibraryDAL.DALUserLogin>();
+builder.Services.AddHttpClient();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ User/wwwroot/js/hide_show_pass.js
@@ -0,0 +1,17 @@
+﻿function myFunction() {
+    var x = document.getElementById("form3Example4");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function myFunction1() {
+    var x = document.getElementById("form3Example5");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
